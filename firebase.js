4d9376868e225523ee1cf47df040bbbfcfa743f7
Jons14-beep <johnsjoseph2004@gmail.com>
2025-03-03 00:38:54 +0530

    Firebase config changed
    yeaaa

--- firebase.js
+++ firebase.js
@@ -20,12 +20,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYJBSzQ3IDKZ9Px9Hv66oe8fA5Ux9ElTU",
-  authDomain: "message-me-844d1.firebaseapp.com",
-  projectId: "message-me-844d1",
-  storageBucket: "message-me-844d1.appspot.com",
-  messagingSenderId: "319333336997",
-  appId: "1:319333336997:web:2906d771a014f650acca4c",
+  apiKey: "AIzaSyDHnjg95feqwM8C4fm28W31morPKwN54L0",
+  authDomain: "ktu-hub-c2f61.firebaseapp.com",
+  projectId: "ktu-hub-c2f61",
+  storageBucket: "ktu-hub-c2f61.firebasestorage.app",
+  messagingSenderId: "51416161248",
+  appId: "1:51416161248:web:927169d4107c5c3fe82985",
+  measurementId: "G-EDGTNH846D"
 };
 
 const app = initializeApp(firebaseConfig);
